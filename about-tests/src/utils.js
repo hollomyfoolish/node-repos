f088af9af3e8526450203d5f23f71ef75b6e18cd
Allen Gong <hollomyfoolish@163.com>
2019-09-25 10:43:22 +0800

    jasmine test

--- about-tests/src/utils.js
+++ about-tests/src/utils.js
@@ -2,4 +2,8 @@ const utils = {
     isUserOldEnough: (age) => age >= 18
 };
 
+setTimeout(() => console.log('timeout'), 0);
+Promise.resolve().then(() => console.log('resolved'));
+process.nextTick(() => console.log('next tick'));
+console.log('start');
 module.exports = utils;
